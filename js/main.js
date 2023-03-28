9827841b4dae5fbca35f700f0efaa4dfe01030f8
Pedro Alves <augustopedroalves@gmail.com>
2023-03-27 23:01:51 -0300

    Manipulaçao do DOM com JavaScript completa

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-const numerosuporte = document.querySelectorAll('#bt-suporte')
+const numerosuporte = document.querySelectorAll('.bt-suporte')
 const printsuporte = document.querySelector('#n-selecionado')
+const submit = document.querySelector('#bt-submit')
+const box1 = document.querySelector('.box-1')
+const box2 = document.querySelector('.box-2')
 
-numerosuporte.addEventListener("click", (item) => {
-   console.log(item.value);
+submit.disabled = true;
+
+numerosuporte.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        printsuporte.innerHTML = btn.innerHTML
+        submit.disabled = false;
+    })
 })
 
-
-
+submit.addEventListener("click", () => {
+    box1.style.display = "none"
+    box2.style.display = "block"
+})
